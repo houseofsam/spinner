51c6a47e6b6e1915f3dee6b61dad1a710e009f84
houseofsam <samxsaid@gmail.com>
2021-11-30 13:26:37 +0000

    spinner 2

---
+++ spinner2.js
@@ -0,0 +1,10 @@
+const spinner = '\r|   \r/   \r-   \r\\   \r|   \r/   \r-   \r\\   \r'
+
+let time = 100;
+
+for (let i = 0; i < spinner.length; i++) {
+  time += 200;
+  setTimeout(() => process.stdout.write(spinner[i]), time);
+}
+
+setTimeout(() => process.stdout.write('\n'), time);
